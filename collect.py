b27b7ab7f6ff55e758b632303d3dff0f36861c69
Nguyen Qui Hieu <nqhieu2001@gmail.com>
2015-06-27 03:15:29 +0800

    update with content-disposition parsing. todo:hex-string decoding

--- collect.py
+++ collect.py
@@ -4,11 +4,11 @@ import requests
 from urlparse import urlparse
 from os.path import splitext,basename,isdir
 from os import makedirs
+import re
 
 
-def crawl(topic_id,folder_name):
+def crawl(topic_id,folder_name,page = 0):
 	base_url = "http://onehallyu.com/index.php?showtopic=%s&page=%s"
-	page = 0
 	#make the directory if not exist
 	if not isdir(folder_name):
 		makedirs(folder_name)
@@ -24,7 +24,7 @@ def crawl(topic_id,folder_name):
 		#end if no more page
 		if r.status_code != 200:
 			break
-
+		print "crawling page %s" %page,
 		soup = BeautifulSoup(r.text)
 		#retrieve all links in the page
 		image_tag = soup.findAll("img")
@@ -46,12 +46,18 @@ def crawl(topic_id,folder_name):
 		for i in image_links:
 			r = requests.get(i, stream=True)
 			if r.status_code == 200:
-				filename = splitext(basename(urlparse(i).path))
-				with open(path + filename[0]+filename[1], 'wb') as f:
+				try:
+					filename = re.findall("filename=(\S+)", r.headers['Content-Disposition'])[0][1:-1]
+				except:
+					filename = splitext(basename(urlparse(i).path))
+					filename = filename[0] + filename[1]
+				with open(path + filename, 'wb') as f:
 					for chunk in r.iter_content(1024):
 						f.write(chunk)
+		print "images saved: %s" % len(image_links),
+		print "DONE."
 
 #modify this line to collect data
 #    crawl(topic_id,folder_name)
 # this will crawl the topic with the correcponding id, and save all the images into folder 'folder_name', arranged by page number
-crawl(88119,"irene")
+crawl(88119,"irene",7)
